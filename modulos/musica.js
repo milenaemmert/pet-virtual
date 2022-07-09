be4d33422c350d110801a8eaf061b385566f28cc
Milena Emmert <milena.emmert@outlook.com>
2022-07-09 17:41:24 -0300

    Botões com ícones: play, pause e stop

--- modulos/musica.js
+++ modulos/musica.js
@@ -1,10 +1,15 @@
 export function tocarMusica() {
     const estiloMusical = document.getElementById('estiloMusical')
-    console.log(estiloMusical.value)
     const tocar = document.querySelector('#tocar')
-    const parar = document.querySelector('#parar')
+    
     let musicaTocando
+    let musicaPausada = false
     tocar.addEventListener('click', function() {
+        tocar.classList.add('display-none')
+        const pausar = document.querySelector('#pausar')
+        pausar.classList.remove('display-none')
+        //imgTocar.setAttribute('src', '../imagens/pause.png')
+        
         const audios = document.querySelectorAll('.audio')
         audios.forEach(function(estilo) {
             let idDoEstilo = estilo.id
@@ -13,10 +18,33 @@ export function tocarMusica() {
                 musicaASerTocada.play()
                 musicaTocando = musicaASerTocada
             }
-        })      
+        })
+        
+        pausar.addEventListener('click', () => {
+            tocar.classList.remove('display-none')
+            pausar.classList.add('display-none')
+            musicaTocando.pause()
+            !musicaPausada
+        })
+
+        //funcionou aqui, dentro desse clique, nao entendi bem o pq (voltar aqui depois!!!)
+        musicaTocando.addEventListener('ended', function() {
+            tocar.classList.remove('display-none')
+            pausar.classList.add('display-none')
+        })
     })
+
+    const parar = document.querySelector('#parar')
     parar.addEventListener('click', function() {
+        
         musicaTocando.pause()
         musicaTocando.currentTime = 0
-    })  
+
+        if(!musicaPausada) {
+            tocar.classList.remove('display-none')
+            pausar.classList.add('display-none')
+        }
+    })
+
+    
 }
