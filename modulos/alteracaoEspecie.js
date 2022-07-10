320e1fdee5ab54e959d097d1a250c020a5b438b2
Milena Emmert <milena.emmert@outlook.com>
2022-07-10 03:45:47 -0300

    Consertando problema ao carregar vídeo de coelho

--- modulos/alteracaoEspecie.js
+++ modulos/alteracaoEspecie.js
@@ -4,7 +4,6 @@ export function alterarEspecie(petSelecionado) {
     const especie = document.querySelector('#especie')
 
     const especieEncontrada = linkEspecies.find(obj => obj.pet === petSelecionado)
-    console.dir('especie encontrada' + especieEncontrada)
     especie.setAttribute('href', especieEncontrada.wiki)
     
     especie.textContent = especieEncontrada.texto
