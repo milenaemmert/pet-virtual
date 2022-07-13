813f8c91addb619a4df4ea9992965a5c0c39b750
Milena Emmert <milena.emmert@outlook.com>
2022-07-13 01:16:25 -0300

    Removendo código comentado

--- modulos/selecaoPet.js
+++ modulos/selecaoPet.js
@@ -2,13 +2,6 @@ import { alterarEspecie } from "./alteracaoEspecie.js"
 
 export function selecionarPet() {
     const petSelecionado = document.querySelector('input[name="pets"]:checked').value
-
-    /* 
-    //usando o find:
-    const pets = document.getElementsByName('pets')
-    const petSelecionado = Object.values(pets)
-        .find(pet => pet.checked).value   
-    */
    
     alterarEspecie(petSelecionado)
     const img = document.querySelector('.pet-imagem')
