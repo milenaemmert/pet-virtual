173f27dd59efabc23631bda95c9b56bfe634e50d
Milena Emmert <milena.emmert@outlook.com>
2022-07-18 09:49:18 -0300

    Consertando problema ao clicar em primeiro radio

--- modulos/alteracaoVideo.js
+++ modulos/alteracaoVideo.js
@@ -1,16 +1,28 @@
 import { linkEspecies } from "../main.js"
 
-export function alterarVideoYoutube(especieEncontrada) {
+(function () {
+    const primeiroPet = document.querySelector('input[name="pets"]')
+    primeiroPet.setAttribute('checked', true)
+})()
+
+export function alterarVideoYoutube(petSelecionado) {
     const iframe = document.querySelector('iframe')
-    
-    iframe.setAttribute('src', especieEncontrada.youtube)
+    iframe.setAttribute('src', petSelecionado.youtube)
 }
 
-const radioButton = document.getElementsByName('pets')
-radioButton.forEach(pet => {
-    pet.onclick = function() {
-        const nomePet = pet.value
-        const petSelecionado = linkEspecies.find(obj => obj.pet === nomePet)
-        alterarVideoYoutube(petSelecionado)
-    }
-})
+function capturarPetRadio() {
+    const petChecked = document.querySelector('input[name="pets"]:checked').value
+    const petSelecionado = linkEspecies.find(obj => obj.pet === petChecked)
+    alterarVideoYoutube(petSelecionado)
+}
+
+window.onload = () => {
+    capturarPetRadio()
+}
+
+const petChecked = document.querySelectorAll('input[name="pets"]')
+petChecked.forEach(pet => {
+    pet.addEventListener('click', (e) => {
+        capturarPetRadio()
+    })
+}) 
